bd8358bc12f8dcc41b92177d69488e71bd4e69d0
damzyadekanle-bit <damzyadekanle@gmail.com>
2026-05-31 19:22:04 -0700

    Add local daycare gallery images

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  if (this.getAttribute('action')) {
+    return;
+  }
+
+  e.preventDefault();
+
+  var formData = new FormData(this);
+  var name = formData.get('name');
+  var email = formData.get('email');
+  var message = formData.get('message');
+  var subject = encodeURIComponent('Tour request from ' + name);
+  var body = encodeURIComponent(
+    'Name: ' + name + '\n' +
+    'Email: ' + email + '\n\n' +
+    message
+  );
+
+  window.location.href = 'mailto:lumbeazdaycare@gmail.com?subject=' + subject + '&body=' + body;
+  this.reset();
+});
